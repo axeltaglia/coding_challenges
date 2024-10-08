80dd431f650a07397af0eee4683d54f5e2afc824
axeltaglia <axeltaglia@gmail.com>
2024-10-08 10:35:50 -0300

    minimumIndexSumOfTwoLists enhancement

--- challenges/minimum_index_sum_of_two_lists/minimumIndexSumOfTwoLists.go
+++ challenges/minimum_index_sum_of_two_lists/minimumIndexSumOfTwoLists.go
@@ -1,39 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println(findRestaurant([]string{"Shogun", "Tapioca Express", "Burger King", "KFC"}, []string{"Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"}))
 }
 func findRestaurant(list1 []string, list2 []string) []string {
-	hotelFreqs := make(map[string]int, 0)
-	acumPositions := make(map[string]int, 0)
+	indexSums := make(map[string]int, 0)
 	for pos, hotel := range list1 {
-		hotelFreqs[hotel]++
-		acumPositions[hotel] += pos
-	}
-
-	for pos, hotel := range list2 {
-		hotelFreqs[hotel]++
-		acumPositions[hotel] += pos
-	}
-
-	min := 100000000
-	for hotel, freq := range hotelFreqs {
-		if freq == 2 {
-			if acumPositions[hotel] < min {
-				min = acumPositions[hotel]
-			}
-		} else {
-			acumPositions[hotel] = -1
-		}
+		indexSums[hotel] = pos
 	}
 
 	output := make([]string, 0)
-
-	for hotel, acumPosition := range acumPositions {
-		if acumPosition == min {
-			output = append(output, hotel)
+	min := math.MaxInt32
+	for i, hotel := range list2 {
+		if _, exists := indexSums[hotel]; exists {
+			indexSum := i + indexSums[hotel]
+			if indexSum < min {
+				min = indexSum
+				output = []string{hotel}
+			} else if indexSum == min {
+				output = append(output, hotel)
+			}
 		}
 	}
 
